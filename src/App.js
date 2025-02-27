cffd0542597d731757eb701576cc6bdcd7cd8656
meganarmas <meganarmas@gmail.com>
2025-02-27 13:45:41 -0600

    publishing this publicly now as this was just a fun project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import { useEffect, useReducer } from "react";
-import Header from "./Header";
-import Main from "./Main";
-import Loader from "./Loader";
-import Error from "./Error";
-import StartScreen from "./StartScreen"
-import Question from "./Question";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Loader from "./components/Loader";
+import Error from "./components/Error";
+import StartScreen from "./components/StartScreen"
+import Question from "./components/Question";
+import NextButton from "./components/NextButton";
+import Progress from "./components/Progress";
 
 const initialState = {
   questions: [],
-  status: 'loading'
+  status: 'loading',
+  index: 0,
+  answer: null,
+  points: 0,
+  highscore: 0
 }
 
 function reducer(state, action) {
@@ -19,26 +25,43 @@ function reducer(state, action) {
         questions: action.payload,
         status: "ready",
       };
+
       case 'dataFailed':
         return{
           ...state, 
           status: "error",
         };
 
-      case 'start':
-        return { ...state, status: "active "};
+      case "start":
+        return { 
+          ...state, 
+          status: "active"
+        };
+
+      case 'newAnswer':
+        const question = state.questions.at(state.index);
+
+        return {
+          ...state, 
+          answer: action.payload, 
+          points: action.payload === question.correctOption ? state.points + question.points : state.points,};
+
+      case 'nextQuestion':
+        return {
+          ...state,
+          index: state.index + 1,
+          answer: null
+        };
+
       default:
         throw new Error('Action unknown');
   }
 }
 
-
-
 export default function App () {
-
-  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
-
+  const [{ questions, status, index, answer, points, highscore, secondsRemaining }, dispatch,] = useReducer(reducer, initialState);
   const numQuestions = questions.length;
+  const maxPossiblePoints = questions.reduce((prev, cur) => prev + cur.points, 0)
 
 
   useEffect(function() {
@@ -48,16 +71,35 @@ export default function App () {
       .catch((err)=> dispatch({ type: 'dataFailed' }));
   }, []);
 
-  return <div>
+  return (
     <div className="app">
       <Header />
       <Main className="main">
         {status === "loading" && <Loader />}
         {status === "error" && <Error />}
-        {status === "ready" && <StartScreen numQuestions = {numQuestions} dispatch={dispatch} />}
-        {status === "active" && <Question />}
+        {status === "ready" && (
+          <StartScreen numQuestions = {numQuestions} dispatch={dispatch} />)}
+        {status === "active" && 
+        (<>
+        <Progress 
+        index={index} 
+        numQuestions={numQuestions} 
+        points={points} 
+        maxPossiblePoints={maxPossiblePoints}
+        answer={answer}
+        />
+        <Question 
+        question={questions[index]} 
+        dispatch={dispatch} 
+        answer={answer} />
+        <NextButton 
+        dispatch={dispatch} 
+        answer={answer}/>
+        </>
+        )}
+
       </Main>
 
     </div>
-  </div>
+  )
 }
--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -7,3 +7,5 @@ function StartScreen ({ numQuestions, dispatch }) {
         </div>
     )
 }
+
+export default StartScreen;
